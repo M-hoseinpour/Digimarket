1cbb6098c5412cad884696ee900b0de585d06e02
Mohammad Hoseinpour <88222566+M-hoseinpour@users.noreply.github.com>
2022-03-28 11:03:41 +0430

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -49,7 +49,7 @@ function App() {
           <Route path="/login">
             <Login />
           </Route>
-          <Route exact path="/Digimarket">
+          <Route exact path="/">
             <Header />
             <Main />
           </Route>
